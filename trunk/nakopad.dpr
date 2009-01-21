636c7919daa85e897c57eed810d7d99f31c16f89
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2009-01-21 17:52:47 +0000

    なでしこエディタのキー管理を複雑化した。

--- trunk/nakopad.dpr
+++ trunk/nakopad.dpr
@@ -36,7 +36,8 @@ uses
   CryptUtils in 'hi_unit\CryptUtils.pas',
   EasyMasks in 'hi_unit\EasyMasks.pas',
   nkf in 'hi_unit\NKF.PAS',
-  unit_rewrite_icon in 'hi_unit\unit_rewrite_icon.pas';
+  unit_rewrite_icon in 'hi_unit\unit_rewrite_icon.pas',
+  SHA1 in 'hi_unit\Sha1.pas';
 
 {$R *.res}
 
