2387c559a91667f1778f0b1b4fc6692a052519d4
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2009-07-03 15:43:43 +0000

    -YZ1/ZIP形式の「圧縮」命令で、サブフォルダにある同名ファイルが全て圧縮される問題を修正(@175)。(但しCABは修正不可能のため放置)(r148)

--- trunk/nadesiko_version.pas
+++ trunk/nadesiko_version.pas
@@ -4,8 +4,8 @@ interface
 
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
-  NADESIKO_VER  = '1.523';
-  NADESIKO_DATE = '2009/06/04';
+  NADESIKO_VER  = '1.524';
+  NADESIKO_DATE = '2009/07/04';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
 implementation
