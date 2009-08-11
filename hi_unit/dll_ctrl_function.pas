3f4891a69ff35b0a182bee8abdd9e8198ac79962
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2009-08-11 02:19:02 +0000

    -「メモリトータルサイズ取得」命令で4GBを超えるメモリに対応(r176)

--- hi_unit/dll_ctrl_function.pas
+++ hi_unit/dll_ctrl_function.pas
@@ -815,10 +815,82 @@ begin
   Result := hi_newInt(g.dwMemoryLoad);
 end;
 
+//-------------------------------------------------------
+// for "GlobalMemoryStatusEx()"
+//-------------------------------------------------------
+type
+  SIZE_T = Cardinal; 
+  {$EXTERNALSYM SIZE_T} 
+  DWORDLONG = Int64;  // ULONGLONG 
+  {$EXTERNALSYM DWORDLONG} 
+
+type 
+  PMemoryStatus = ^TMemoryStatus; 
+  LPMEMORYSTATUS = PMemoryStatus; 
+  {$EXTERNALSYM LPMEMORYSTATUS} 
+  _MEMORYSTATUS = packed record 
+    dwLength       : DWORD; 
+    dwMemoryLoad   : DWORD; 
+    dwTotalPhys    : SIZE_T; 
+    dwAvailPhys    : SIZE_T; 
+    dwTotalPageFile: SIZE_T; 
+    dwAvailPageFile: SIZE_T; 
+    dwTotalVirtual : SIZE_T; 
+    dwAvailVirtual : SIZE_T; 
+  end; 
+  {$EXTERNALSYM _MEMORYSTATUS} 
+  TMemoryStatus = _MEMORYSTATUS; 
+  MEMORYSTATUS = _MEMORYSTATUS; 
+  {$EXTERNALSYM MEMORYSTATUS} 
+
+type 
+  PMemoryStatusEx = ^TMemoryStatusEx; 
+  LPMEMORYSTATUSEX = PMemoryStatusEx; 
+  {$EXTERNALSYM LPMEMORYSTATUSEX} 
+  _MEMORYSTATUSEX = packed record 
+    dwLength        : DWORD; 
+    dwMemoryLoad    : DWORD; 
+    ullTotalPhys    : DWORDLONG; 
+    ullAvailPhys    : DWORDLONG; 
+    ullTotalPageFile: DWORDLONG; 
+    ullAvailPageFile: DWORDLONG; 
+    ullTotalVirtual : DWORDLONG; 
+    ullAvailVirtual : DWORDLONG; 
+  end; 
+  {$EXTERNALSYM _MEMORYSTATUSEX} 
+  TMemoryStatusEx = _MEMORYSTATUSEX; 
+  MEMORYSTATUSEX = _MEMORYSTATUSEX; 
+  {$EXTERNALSYM MEMORYSTATUSEX} 
+
+//-------------------------------------------------------
+procedure GlobalMemoryStatus(var lpBuffer: TMemoryStatus); stdcall;
+  external kernel32;
+{$EXTERNALSYM GlobalMemoryStatus}
+
+function GlobalMemoryStatusEx(var lpBuffer: TMemoryStatusEx): BOOL; stdcall;
+type
+  TFNGlobalMemoryStatusEx = function(var msx: TMemoryStatusEx): BOOL; stdcall;
+var
+  FNGlobalMemoryStatusEx: TFNGlobalMemoryStatusEx;
+begin
+  FNGlobalMemoryStatusEx := TFNGlobalMemoryStatusEx(
+    GetProcAddress(GetModuleHandle(kernel32), 'GlobalMemoryStatusEx'));
+  if not Assigned(FNGlobalMemoryStatusEx) then
+  begin
+    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+    Result := False;
+  end
+  else
+    Result := FNGlobalMemoryStatusEx(lpBuffer);
+end;
+//-------------------------------------------------------
+
+
 function cmd_memory_status(h: DWORD): PHiValue; stdcall;
 var
   g:TMemoryStatus;
   os:_OSVERSIONINFOA;
+  Status: TMemoryStatusEx;
 begin
   ZeroMemory(@os, SizeOf(os));
   os.dwOSVersionInfoSize := SizeOf(os);
@@ -829,8 +901,12 @@ begin
   begin
     //TODO: 将来的に対応する
     //GlobalMemoryStatusEx <-- 4G超えるメモリサイズを取得可能
-    GlobalMemoryStatus(g);
-    Result := hi_newInt(g.dwTotalPhys);
+    //GlobalMemoryStatus(g);
+    //Result := hi_newInt(g.dwTotalPhys);
+    ZeroMemory(@Status, SizeOf(TMemoryStatusEx));
+    Status.dwLength := SizeOf(TMemoryStatusEx);
+    GlobalMemoryStatusEx(Status);
+    Result := hi_newFloat(Status.ullTotalPhys);
   end else begin
     GlobalMemoryStatus(g);
     Result := hi_newInt(g.dwTotalPhys);
