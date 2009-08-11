f896e146afa1bd9ae00cd0276c57864250bd1a5c
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2009-08-11 04:39:23 +0000

    -「ブラウザINPUT値設定」「ブラウザINPUT値取得」「ブラウザFORM送信」「ブラウザHTML書換」を追加。(r175)(r177)

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -4,7 +4,7 @@ interface
 
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
-  NADESIKO_VER  = '1.5251';
+  NADESIKO_VER  = '1.5252';
   NADESIKO_DATE = '2009/08/10';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
