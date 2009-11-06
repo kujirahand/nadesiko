7f914f8eefbcd63f37f14a1424e712fc16f4fa07
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2009-11-06 17:02:23 +0000

    - rs232c.nako が動かない問題を修正(r207)(@362)

--- trunk/hi_unit/dll_rs232c_function.pas
+++ trunk/hi_unit/dll_rs232c_function.pas
@@ -11,7 +11,8 @@ type
     rxdata_cnt: Integer;       // 追加
     rxdata_packet: String;     // 追加
   public
-    group: PHiValue;
+    instanceName: string;
+    oid: Integer;
     procedure FFOnBREAK(Sender: TObject);
     procedure FFOnCTS(Sender: TObject);
     procedure FFOnDSR(Sender: TObject);
@@ -34,56 +35,79 @@ procedure RegistFunction;
 
 implementation
 
+var rs232c_list : array of Trs232cN;
+
+function getPropStr(g:Trs232cN; prop: string): string;
+var
+  s: string;
+  p: PHiValue;
+begin
+  Result := '';
+  s := g.instanceName + '→' + prop;
+  nako_evalEx(PChar(s), p);
+  if p <> nil then
+  begin
+    Result := hi_str(p);
+    nako_var_free(p);
+  end;
+end;
+
+function getPropInt(g:Trs232cN; prop: string; def: Integer = 0): Integer;
+var s: string;
+begin
+  s := getPropStr(g, prop);
+  Result := StrToIntDef( s, def );
+end;
 
 function rs232c_cmd(arg: DWORD): PHiValue; stdcall;
 var
-  g, p: PHiValue;
   cmd, v: string;
-  Frs232c: Trs232cN;
+  Frs232c, g: Trs232cN;
+  oid: Integer;
 begin
   Result := nil;
+  if rs232c_list = nil then SetLength(rs232c_list, 255);
+
   // 引数の取得
-  g   := nako_getFuncArg(arg, 0);
-  cmd := hi_str( nako_getFuncArg(arg, 1) );
-  v   := hi_str( nako_getFuncArg(arg, 2) );
+  oid := getArgInt(arg, 0, True);
+  cmd := getArgStr(arg, 1);
+  v   := getArgStr(arg, 2);
 
   // コマンドの解析
   if cmd = 'create' then
   begin
-    p := nako_group_findMember(g, 'オブジェクト');
-    if p = nil then raise Exception.Create('オブジェクトが特定できません。');
     //
     Frs232c := Trs232cN.Create(nil);
-    Frs232c.group := g;
-    hi_setInt(p, Integer(Frs232c));
+    Frs232c.instanceName := v;
+    rs232c_list[ oid ] := Frs232c;
     //
     Exit;
   end;
 
-  p := nako_group_findMember(g, 'オブジェクト');
-  if p = nil then raise Exception.Create('オブジェクトが特定できません。');
-  Frs232c := Trs232cN(hi_int(p));
+  g := rs232c_list[ oid ];
+  if g = nil then raise Exception.Create('オブジェクトが特定できません。');
+  Frs232c := Trs232cN(g);
 
   if cmd = 'open' then
   begin
     // 設定
-    Frs232c.bps         := hi_int(nako_group_findMember(g, 'BPS'));
-    Frs232c.charbit     := hi_int(nako_group_findMember(g, 'CHARBIT'));
-    Frs232c.stopbit     := hi_str(nako_group_findMember(g, 'STOPBIT'));
-    Frs232c.paritymode  := hi_str(nako_group_findMember(g, 'PARITYMODE'));
-    Frs232c.rxtimeout   := hi_int(nako_group_findMember(g, 'タイムアウト'));
-    Frs232c.txtimeout   := hi_int(nako_group_findMember(g, 'タイムアウト'));
-    Frs232c.portname    := hi_str(nako_group_findMember(g, 'ポート'));
-    Frs232c.packetsize  := hi_int(nako_group_findMember(g, 'パケットサイズ'));
-    Frs232c.XonLim      := hi_int(nako_group_findMember(g, 'XonLim'));
-    Frs232c.XoffLim     := hi_int(nako_group_findMember(g, 'Xoff'));
-    Frs232c.XonChar     := Char(hi_int(nako_group_findMember(g,'XonChar')));
-    Frs232c.XoffChar    := Char(hi_int(nako_group_findMember(g,'XoffChar')));
-    Frs232c.ErrorChar   := Char(hi_int(nako_group_findMember(g,'ErrorChar')));
-    Frs232c.EvtChar     := Char(hi_int(nako_group_findMember(g,'EvtChar')));
-    Frs232c.EofChar     := Char(hi_int(nako_group_findMember(g,'EofChar')));
+    Frs232c.bps         := getPropInt(g, 'BPS', 9600);
+    Frs232c.charbit     := getPropInt(g, 'CHARBIT', 8);
+    Frs232c.stopbit     := getPropStr(g, 'STOPBIT');
+    Frs232c.paritymode  := getPropStr(g, 'PARITYMODE');
+    Frs232c.rxtimeout   := getPropInt(g, 'タイムアウト', 3000);
+    Frs232c.txtimeout   := getPropInt(g, 'タイムアウト', 3000);
+    Frs232c.portname    := getPropStr(g, 'ポート');
+    Frs232c.packetsize  := getPropInt(g, 'パケットサイズ', 0);
+    Frs232c.XonLim      := getPropInt(g, 'XonLim', 17);
+    Frs232c.XoffLim     := getPropInt(g, 'Xoff', 19);
+    Frs232c.XonChar     := Char((getPropInt(g,'XonChar')));
+    Frs232c.XoffChar    := Char((getPropInt(g,'XoffChar')));
+    Frs232c.ErrorChar   := Char((getPropInt(g,'ErrorChar')));
+    Frs232c.EvtChar     := Char((getPropInt(g,'EvtChar')));
+    Frs232c.EofChar     := Char((getPropInt(g,'EofChar')));
 
-    if not Frs232c.rsopen then raise Exception.Create('RS232Cのポートが開けません。');
+    if not Frs232c.rsopen then raise Exception.Create('RS232Cのポート『'+Frs232c.portname+'』が開けません。');
   end else
   if cmd = 'close' then
   begin
@@ -102,7 +126,7 @@ end;
 procedure RegistFunction;
 begin
   //4400-4499
-  AddFunc('RS232C_COMMAND','{グループ}G,CMD,V',  4400, rs232c_cmd, 'RS232Cの設定を行う', 'RS232C_COMMAND');
+  AddFunc('RS232C_COMMAND', 'OID,CMD,V',  4400, rs232c_cmd, 'RS232Cの設定を行う', 'RS232C_COMMAND');
 end;
 
 
@@ -185,7 +209,7 @@ end;
 
 procedure Trs232cN.FFOnPACKET(Sender: TObject);
 var
-  s: string;
+  s, e: string;
   p: PHiValue;
   c: Char;
   max_data_len: integer;
@@ -205,11 +229,10 @@ begin
     if (max_data_len >= packetsize) then begin
       s := Copy(rxdata_packet,1,packetsize);
 
-      p := nako_group_findMember(group, '受信データ');
-      if p = nil then Exit;
-
-      hi_setStr(p, s);
-
+      //
+      e := self.instanceName + '→受信データ=『' + s + '』;';
+      nako_evalEx(PChar(e), p);
+      nako_var_free(p);
       OnEvent('パケット受信した時');
 
       rxdata_packet := '';
@@ -221,7 +244,7 @@ end;
 
 procedure Trs232cN.FFOnRXCHAR(Sender: TObject);
 var
-  s: string;
+  s, e: string;
   p: PHiValue;
   c: Char;
 begin
@@ -240,10 +263,9 @@ begin
   if s = '' then
     Exit;
 
-  p := nako_group_findMember(group, '受信データ');
-  if p = nil then Exit;
-
-  hi_setStr(p, s);
+  e := self.instanceName + '→受信データ=『' + s + '』;';
+  nako_evalEx(PChar(e), p);
+  nako_var_free(p);
 
   OnEvent('受信した時');
 end;
@@ -255,21 +277,17 @@ end;
 
 procedure Trs232cN.OnEvent(ename: string);
 var
+  e: string;
   p: PHiValue;
 begin
-  p := nako_group_findMember(group, PChar(ename));
-  if p = nil then Exit;
-  //if p^.VType = varFunc then
-  if (p<>nil)and(p.ptr <> nil) then
-  begin
-    nako_continue;
-    nako_group_exec(group, PChar(ename));
-  end;
+  e := Self.instanceName + 'の' + ename + ';';
+  nako_evalEx(PChar(e), p);
+  if p <> nil then nako_var_free(p);
 end;
 
 procedure Trs232cN.FFOnRXFLAG(Sender: TObject);
 var
-  s: string;
+  s, e: string;
   p: PHiValue;
 begin
   if rxdatalen > 0 then
@@ -277,11 +295,28 @@ begin
     SetLength(s, rxdatalen);
     self.rsread(s[1], Length(s));
 
-    p := nako_group_findMember(group, '受信データ');
-    if p = nil then Exit;
-    hi_setStr(p, s);
+    e := self.instanceName + '→受信データ=『' + s + '』;';
+    nako_evalEx(PChar(e), p);
+    nako_var_free(p);
+
     OnEvent('イベント文字受信した時');
   end;
 end;
+
+procedure free_rs232c_list;
+var
+  i: Integer;
+begin
+  if rs232c_list = nil then Exit;
+  for i := 0 to High(rs232c_list) do
+  begin
+    FreeAndNil(rs232c_list[i]);
+  end;
+end;
+
+initialization
+
+finalization
+  free_rs232c_list;
 
 end.
