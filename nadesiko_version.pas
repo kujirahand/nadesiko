d3dd20ef5d664ff41e3ff53c0dadf1d089c40c21
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2009-11-06 18:11:28 +0000

    - 実行ファイル作成時、暗号化をチェックしても過去のなでしこで解読される怖れがある問題に対処(r210)(@364)

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -5,7 +5,7 @@ interface
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
   NADESIKO_VER  = '1.5323';
-  NADESIKO_DATE = '2009/10/08';
+  NADESIKO_DATE = '2009/11/07';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
 implementation
