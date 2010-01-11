cc3619ea63e3a808f3a8dee633e1f44330a2ce53
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2010-01-11 10:42:26 +0000

    なでしこエディタ用マクロに、selectall/selectnoneコマンドを追加(r221)。

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -5,7 +5,7 @@ interface
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
   NADESIKO_VER  = '1.5325';
-  NADESIKO_DATE = '2009/12/06';
+  NADESIKO_DATE = '2010/01/01';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
 implementation
