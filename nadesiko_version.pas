3b5d522f923a6a18011e23a90cefaf0b7f9b7488
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2013-04-21 03:22:41 +0000

    -「メール受信」で添付ファイルの保存に失敗する問題を修正(@667)(r284)

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -4,8 +4,8 @@ interface
 
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
-  NADESIKO_VER  = '1.53354';
-  NADESIKO_DATE = '2013/04/04';
+  NADESIKO_VER  = '1.53355';
+  NADESIKO_DATE = '2013/04/21';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
   // REPORT : LocalAppData
