a544de6cf061be83a99c6bfd07866566c2889f6e
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2014-01-04 02:11:00 +0000

    Uメモのサンプルを追加。「OS」でWindows8.1を判定できるように修正。

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -4,8 +4,8 @@ interface
 
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
-  NADESIKO_VER  = '1.539';
-  NADESIKO_DATE = '2013/06/26';
+  NADESIKO_VER  = '1.540';
+  NADESIKO_DATE = '2014/01/04';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
   // REPORT : LocalAppData
