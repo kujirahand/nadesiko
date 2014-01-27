00b257b3cd1247ef5f0819393d663ea85a0a064a
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2014-01-27 17:14:17 +0000

    -TエディタでCPP/Java/Perlでカラーリングが利かない問題を修正(r315)(@636)

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -4,8 +4,8 @@ interface
 
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
-  NADESIKO_VER  = '1.540';
-  NADESIKO_DATE = '2014/01/04';
+  NADESIKO_VER  = '1.510';
+  NADESIKO_DATE = '2014/01/28';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
   // REPORT : LocalAppData
