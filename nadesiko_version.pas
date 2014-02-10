7a1476a60bc9d4e3bd656602a232e51ff5cb31cb
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2014-02-10 08:05:50 +0000

    -(r313)のエンバグを修正(r314)(@714)(@715)

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -4,8 +4,8 @@ interface
 
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
-  NADESIKO_VER  = '1.510';
-  NADESIKO_DATE = '2014/01/28';
+  NADESIKO_VER  = '1.542';
+  NADESIKO_DATE = '2014/02/10';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
   // REPORT : LocalAppData
