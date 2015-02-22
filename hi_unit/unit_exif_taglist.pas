36d79ce8bbe202b8c3a50750706e0149745b44c0
mindsral@gmail.com <mindsral@gmail.com@63f267ec-9694-11dd-a226-674c8a694a6d>
2015-02-22 06:13:38 +0000

    -JPEG/EXIFのEXIT2.3までのEXIFタグに対応。 -JPEG/EXIFのGPS情報の取得に対応。 -JPEG/EXIFのバイト順が'MM'の場合に値が正しく取得できない問題に対応。

--- hi_unit/unit_exif_taglist.pas
+++ hi_unit/unit_exif_taglist.pas
@@ -5,21 +5,65 @@ const
   F_IFD0   = 0;
   F_SubIFD = 1;
   F_IIFD   = 2;
-  F_IFDx   = 3;
+  F_GpsIFD = 3;
+  F_IFDx   = 4;
 const
-  F_EXIF_PLACE: array[0..117]of Integer = (F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx);
-  F_EXIF_TAG  : array[0..117]of Integer = ($010e,$010f,$0110,$0112,$011a,$011b,$0128,$0131,$0132,$013e,$013f,$0211,$0213,$0214,$8298,$8769,$829a,$829d,$8822,$8827,$9000,$9003,$9004,$9101,$9102,$9201,$9202,$9203,$9204,$9205,$9206,$9207,$9208,$9209,$920a,$927c,$9286,$9290,$9291,$9292,$a000,$a001,$a002,$a003,$a004,$a005,$a20e,$a20f,$a210,$a215,$a217,$a300,$a301,$a302,$0001,$0002,$1000,$1001,$1001,$0100,$0101,$0102,$0103,$0106,$0111,$0112,$0115,$0116,$0117,$011a,$011b,$011c,$0128,$0201,$0202,$0211,$0212,$0213,$0214,$00fe,$00ff,$012d,$013b,$013d,$013e,$013f,$0142,$0143,$0144,$0145,$014a,$015b,$828d,$828e,$828f,$83bb,$8773,$8824,$8825,$8828,$8829,$882a,$882b,$920b,$920c,$920d,$9211,$9212,$9213,$9214,$9215,$9216,$9290,$9291,$9292,$a20b,$a20c,$a214);
-  F_EXIF_TYPE : array[0..117]of Integer = (2,2,2,3,5,5,3,2,2,5,5,5,3,5,2,4,5,5,3,3,7,2,2,7,5,10,5,10,10,5,10,3,3,3,5,7,7,2,2,2,7,3,4,4,2,4,5,5,3,5,3,7,7,7,2,7,2,9,9,4,4,3,3,3,4,3,3,4,4,5,5,3,3,4,4,5,3,3,5,4,3,3,2,3,5,5,3,3,4,3,4,7,3,1,5,4,7,2,4,7,3,8,3,5,7,7,4,2,2,3,5,1,2,2,2,5,3,3);
-  F_EXIF_COUNT: array[0..117]of Integer = (0,0,0,1,1,1,1,0,20,2,6,3,1,6,0,1,1,1,1,2,4,20,20,4,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,4,1,1,1,0,1,1,1,1,1,1,1,1,0,4,4,0,1,1,1,1,3,1,1,0,1,1,1,0,1,1,1,1,1,1,3,2,1,6,1,1,3,0,1,2,6,1,1,0,0,0,0,2,0,1,0,0,0,1,0,1,1,1,1,0,0,1,1,0,4,1,4,0,0,0,1,1,1);
-  F_EXIF_NAME : array[0..117]of String = ('ImageDescription',
+  F_EXIF_PLACE: array[0..178]of Integer = (
+F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,F_IFD0,
+F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,
+F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,F_SubIFD,
+F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,F_IIFD,
+F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,F_GpsIFD,
+F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx,F_IFDx);
+  F_EXIF_TAG  : array[0..178]of Integer = (
+// IFD0(20)
+$010e,$010f,$0110,$0112,$011a,$011b,$0128,$012d,$0131,$0132,$013b,$013e,$013f,$0211,$0213,$0214,$8298,$8769,$8825,$ea1c,
+// SubIFD(Private Exif IFD)(39+26)
+$829a,$829d,$8822,$8827,$9000,$9003,$9004,$9101,$9102,$9201,$9202,$9203,$9204,$9205,$9206,$9207,$9208,$9209,$920a,$927c,$9286,$9290,$9291,$9292,$a000,$a001,$a002,$a003,$a004,$a005,$a20e,$a20f,$a210,$a215,$a217,$a300,$a301,$a302,$ea1c,
+$8830,$8831,$8832,$8833,$8834,$8835,$a401,$a402,$a403,$a404,$a405,$a406,$a407,$a408,$a409,$a40a,$a40b,$a40c,$a420,$a430,$a431,$a432,$a433,$a434,$a435,$a500,
+// IIFD(25)
+$0001,$0002,$1000,$1001,$1001,$0100,$0101,$0102,$0103,$0106,$0111,$0112,$0115,$0116,$0117,$011a,$011b,$011c,$0128,$0201,$0202,$0211,$0212,$0213,$0214,
+// GpsIFD(IFD for GPS)(32)
+$0000,$0001,$0002,$0003,$0004,$0005,$0006,$0007,$0008,$0009,$000a,$000b,$000c,$000d,$000e,$000f,$0010,$0011,$0012,$0013,$0014,$0015,$0016,$0017,$0018,$0019,$001a,$001b,$001c,$001d,$001e,$001f,
+// IFDx(37)
+$00fe,$00ff,$013d,$013e,$013f,$0142,$0143,$0144,$0145,$014a,$015b,$828d,$828e,$828f,$83bb,$8773,$8824,$8825,$8828,$8829,$882a,$882b,$920b,$920c,$920d,$9211,$9212,$9213,$9214,$9215,$9216,$9290,$9291,$9292,$a20b,$a20c,$a214);
+  F_EXIF_TYPE : array[0..178]of Integer = (
+// IFD0
+2,2,2,3,5,5,3,3,2,2,2,5,5,5,3,5,2,4,4,7,
+// SubIFD(Private Exif IFD)
+5,5,3,3,7,2,2,7,5,10,5,10,10,5,10,3,3,3,5,7,7,2,2,2,7,3,4,4,2,4,5,5,3,5,3,7,7,7,7,
+3,4,4,4,4,4,3,3,3,5,3,3,3,3,3,3,7,3,2,2,2,5,2,2,2,5,
+// IIFD
+2,7,2,9,9,4,4,3,3,3,4,3,3,4,4,5,5,3,3,4,4,5,3,3,5,
+// GpsIFD
+1,2,5,2,5,1,5,5,2,2,2,5,2,5,2,5,2,5,2,2,5,2,5,2,5,2,5,7,7,2,3,5,
+// IFDx
+4,3,3,5,5,3,3,4,3,4,7,3,1,5,4,7,2,4,7,3,8,3,5,7,7,4,2,2,3,5,1,2,2,2,5,3,3);
+  F_EXIF_COUNT: array[0..178]of Integer = (
+// IFD0
+0,0,0,1,1,1,3,1,0,20,0,2,6,3,1,6,0,1,0,0,
+// SubIFD(Private Exif IFD)
+1,1,1,2,4,20,20,4,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,4,1,1,1,0,1,1,1,1,1,1,1,1,0,0,
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,33,0,0,4,0,0,0,1,
+// IIFD
+4,4,0,1,1,1,1,3,1,1,0,1,1,1,0,1,1,1,1,1,1,3,2,1,6,
+// GpsIFD
+4,2,3,2,3,1,1,3,0,2,2,1,2,1,2,1,2,1,0,2,3,2,3,2,1,2,1,0,0,11,1,1,
+// IFDx
+1,1,1,2,6,1,1,0,0,0,0,2,0,1,0,0,0,1,0,1,1,1,1,0,0,1,1,0,4,1,4,0,0,0,1,1,1);
+  F_EXIF_NAME : array[0..178]of String = (
+// IFD0
+'ImageDescription',
 'Make',
 'Model',
 'Orientation',
 'XResolution',
 'YResolution',
 'ResolutionUnit',
+'TransferFunction',
 'Software',
 'DateTime',
+'Artist',
 'WhitePoint',
 'PrimaryChromaticities',
 'YCbCrCoefficients',
@@ -27,6 +71,10 @@ const
 'ReferenceBlackWhite',
 'Copyright',
 'ExifIFDPointer',
+'GPSInfoIFDPointer',
+'Padding',
+
+// SubIFD(Private Exif IFD)
 'ExposureTime',
 'FNumber',
 'ExposureProgram',
@@ -65,6 +113,35 @@ const
 'FileSource',
 'SceneType',
 'CFAPattern',
+'Padding',
+
+'SensitivityType',
+'StandardOutputSensitivity',
+'RecommendedExposureIndex',
+'ISOSpeed',
+'ISOSpeedLatitydeyyy',
+'ISOSpeedLatitudezzz',
+'CustomRendered',
+'ExposureMode',
+'WhiteBalance',
+'DigitalZoomRatio',
+'FocalLengthIn35mmFilm',
+'SceneCaptureType',
+'GainControl',
+'Contrast',
+'Saturation',
+'Sharpness',
+'DeviceSettingDescription',
+'SubjectDistanceRange',
+'ImageUniqueID',
+'CameraOwnerName',
+'BodySerialNumber',
+'LensSpecification',
+'LensMake',
+'LensModel',
+'LensSerialNumber',
+'Gamma',
+// IIFD
 'InteroperabilityIndex',
 'InteroperabilityVersion',
 'RelatedImageFileFormat',
@@ -90,10 +167,44 @@ const
 'YCbCrSubSampling',
 'YCbCrPositioning',
 'ReferenceBlackWhite',
+
+// GpsIFD
+'GPSVersionID',
+'GPSLatitudeRef',
+'GPSLatitude',
+'GPSLongitudeRef',
+'GPSLongitude',
+'GPSAltitudeRef',
+'GPSAltitude',
+'GPSTimeStamp',
+'GPSSatellites',
+'GPSStatus',
+'GPSMeasureMode',
+'GPSDOP',
+'GPSSpeedRef',
+'GPSSpeed',
+'GPSTrackRef',
+'GPSTrack',
+'GPSImgDirectionRef',
+'GPSImgDirection',
+'GPSMapDatum',
+'GPSDestLatitudeRef',
+'GPSDestLatitude',
+'GPSDestLongitudeRef',
+'GPSDestLongitude',
+'GPSDestBearingRef',
+'GPSDestBearing',
+'GPSDestDistanceRef',
+'GPSDestDistance',
+'GPSProcessingMethod',
+'GPSAreaInformation',
+'GPSDateStamp',
+'GPSDifferential',
+'GPSHPositioningError',
+
+// IFDx
 'NewSubfileType',
 'SubfileType',
-'TransferFunction',
-'Artist',
 'Predictor',
 'WhitePoint',
 'PrimaryChromaticities',
