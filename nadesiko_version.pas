9eda80dbe954ba9a1cb4004027e0b1c3fff197a4
kujirahand <kujirahand@63f267ec-9694-11dd-a226-674c8a694a6d>
2015-02-28 14:28:20 +0000

    「地図表示」「経路表示」命令を追加

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -4,8 +4,8 @@ interface
 
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
-  NADESIKO_VER  = '1.549';
-  NADESIKO_DATE = '2015/02/24';
+  NADESIKO_VER  = '1.550';
+  NADESIKO_DATE = '2015/02/28';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
   // REPORT : LocalAppData
