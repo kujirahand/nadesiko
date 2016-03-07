96ab3839b9d86a5d70d260ec485234b018bbdf5b
kujirahand <web@kujirahand.com>
2016-03-07 12:32:22 +0900

    バージョンアップのため

--- nadesiko_version.pas
+++ nadesiko_version.pas
@@ -4,8 +4,8 @@ interface
 
 const
   //todo 1:ššš‚È‚Å‚µ‚±ƒo[ƒWƒ‡ƒ“
-  NADESIKO_VER  = '1.555';
-  NADESIKO_DATE = '2015/12/19';
+  NADESIKO_VER  = '1.556';
+  NADESIKO_DATE = '2016/03/07';
   NADESIKO_GUID = '8EBAA659-590A-487A-8260-00F32660E014';
 
   // REPORT : LocalAppData
